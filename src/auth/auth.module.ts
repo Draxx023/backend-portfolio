20d20e05a83c03e9652dad416a8a9e58ab36cac8
Draxx023 <ayoub2001@hotmail.it>
2024-01-15 14:27:16 +0100

    cleanup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import * as config from '../config/config.json'
 
 @Module({
